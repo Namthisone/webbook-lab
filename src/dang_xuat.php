b5a4b2ef83a242a0b3735a6ce78be6ee283bd124
Namthisone <namanh6a8@gmail.com>
2026-08-17 21:08:14 +0700

    security: securely terminate login sessions

--- src/dang_xuat.php
+++ src/dang_xuat.php
@@ -1,8 +1,16 @@
 <?php
-session_start();
-// Hủy bỏ toàn bộ session gán cho tài khoản
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+}
+
 session_destroy();
-// Quay trở về trang chủ
-header("Location: dang_nhap.php");
+header('Location: dang_nhap.php');
 exit;
 ?>
